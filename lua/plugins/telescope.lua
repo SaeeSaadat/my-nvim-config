1cf903ef97a79ef14de0b84d117c0c4a32fe5246
Saee <saee@Saee---niologics-MacBook-Pro.local>
2025-01-07 16:12:55 +0100

    some updates

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,10 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
 -- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      keys =
+      {
+          {'<leader>fg', "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
+          {'<leader>ff', "<cmd>Telescope find_files<cr>", desc = "Find file"},
+      },
     }
